36b27cceeeba7453911f3a94abb438f55b60543d
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-03 14:02:55 +0100

    React App Created.

---
+++ client/src/component/login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div>
+      djshsh
+    </div>
+  )
+}
+
+export default login
---
+++ client/src/component/register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function register() {
+  return (
+    <div>
+      jsjs
+    </div>
+  )
+}
+
+export default register
