f58f799289a1f876847b32a78e28e7eca945a381
clartor <clara.tornblom@gmail.com>
2021-11-03 17:22:53 +0100

    form mall klar

---
+++ client/src/components/login/Login.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function Login() {
+  // const username = "";
+  return (
+    <div>
+      login login 
+    {/* <form onSubmit={handleSubmit} >
+        <input ref={username} label="Username:" type="text" />
+        <input ref={email} label="Email:" type="email" />
+        <input ref={password} label="Password:" type="password" />
+        <div>
+          <button style={submitStyle} type="submit">Submit</button>
+        </div>
+      </form> */}
+      
+    </div>
+  )
+}
+
+export default Login
