85a0fcc5f189a70008f1f443a9d6c9e94643c545
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-03 21:33:42 +0100

    Add update todo.

--- server/routes/users.routes.ts
+++ server/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import Express from 'express';
 const UserRouter = Express.Router()
-import { user_signup, user_delete, user_login, user_update, user_veirfy, membersPage, todo_new, todo_delete } from '../controllers/users.controllers';
+import { user_signup, user_delete, user_login, user_update, user_veirfy, membersPage, todo_new, todo_delete, todo_update } from '../controllers/users.controllers';
 
 UserRouter.get('/verifi-email', user_veirfy);
 UserRouter.get("/members/", membersPage);
@@ -8,6 +8,7 @@ UserRouter.post('/signup', user_signup);
 UserRouter.post('/login', user_login);
 UserRouter.post('/todo/new', todo_new);
 UserRouter.patch('/:userId', user_update);
+UserRouter.patch('/todo/update', todo_update);
 UserRouter.delete('/:userId', user_delete);
 UserRouter.delete('/todo/delete', todo_delete);
 
