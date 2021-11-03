d53cc0ff466a687fd72d7f9d62b84e6678b42fe1
clartor <clara.tornblom@gmail.com>
2021-11-03 15:52:23 +0100

    to do klar

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Redirect,
+  Route
 } from "react-router-dom";
 import todo from './component/todo';
 import login from './component/login/Login';
@@ -11,25 +10,123 @@ import register from './component/register';
 
 import './App.css';
 
-function App() {
+function App(props) {
+  // return (
+  //   <Router>
+
+  //     <div className="App">
+  //       <header>
+  //         Dina Sysslor
+  //       </header>
+
+  //       function App(props) {
   return (
-    <Router>
-
-      <div className="App">
-        <header>
-          HEJ
-        </header>
-
-        <Switch>
-          <Route path="/todo" component={todo} />
-          <Route path="/login" component={login} />
-          <Route path="/register" component={register} />
-        </Switch>
-
-
+    <div className="todoapp stack-large">
+      <h1>Missions</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
       </div>
-    </Router>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <label className="todo-label" htmlFor="todo-0">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+              Sleep
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-2" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-2">
+              Repeat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Repeat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
+        {/* <Switch>
+          <Route exakt path="/" component={todo}></Route>
+          <Route path="/login" component={login} /> hemm
+          <Route path="/register" component={register} />
+        </Switch> */}
+
+
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default App;
--- client/src/component/register.js
+++ client/src/component/register.js
@@ -3,6 +3,7 @@ import React from 'react'
 function register() {
   return (
     <div>
+      <h1> Register </h1>
       jsjs
     </div>
   )
