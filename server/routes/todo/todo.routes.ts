8a78e1f515feee3c0d5c995e92ec0409f54ace6e
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-08 23:26:52 +0100

    Import validation checks for user and todo routes.

--- server/routes/todo/todo.routes.ts
+++ server/routes/todo/todo.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import isValidToken from '../../helpers/verifyToken.helpers'
+import isValidToken from '../../middleware/authentication/verifyToken.middleware'
 import { create, deleteOneTodo } from '../../controllers/todos.controller';
+import { validationRequestResults } from '../../middleware/validationErrorHandler/validationErrors.middleware'
+import { createTodoValidationCheck } from '../../middleware/todoValidation/todoValidationCheck.middleware'
 
 
 const Route = Router()
-Route.post("/new", isValidToken, create);
+Route.post("/new", isValidToken, createTodoValidationCheck, validationRequestResults, create);
 Route.delete('/:todoId', isValidToken, deleteOneTodo)
 
 
--- server/routes/users/user.routes.ts
+++ server/routes/users/user.routes.ts
@@ -1,14 +1,28 @@
-import Express from 'express';
-const UserRouter = Express.Router()
-import isValidToken from '../../helpers/verifyToken.helpers'
-import { createUser, verifyUser, loginUser, deleteUser, account } from '../../controllers/users.controller';
+import { Router } from 'express';
+import isValidToken from '../../middleware/authentication/verifyToken.middleware'
+import { validationRequestResults } from '../../middleware/validationErrorHandler/validationErrors.middleware'
+import {
+  userRegisterValidationCheck,
+  userLoginValidationCheck
+}
+  from '../../middleware/userValidation/userValidationCheck.middleware'
+
+import {
+  createUser,
+  verifyUser,
+  loginUser,
+  deleteUser,
+  account
+}
+  from '../../controllers/users.controller';
 
 
-UserRouter.get('/verifi-email', verifyUser);
-UserRouter.get("/account/", isValidToken, account);
-UserRouter.post('/signup', createUser);
-UserRouter.post('/login', loginUser);
-UserRouter.delete('/:userId', deleteUser);
+const Route = Router()
+Route.get("/account/", isValidToken, account);
+Route.get('/verifi-email', verifyUser);
+Route.post('/signup', userRegisterValidationCheck, validationRequestResults, createUser);
+Route.post('/login', userLoginValidationCheck, validationRequestResults, loginUser);
+Route.delete('/:userId', deleteUser);
 
-export default UserRouter
+export { Route as UserRouter }
 
