08e2972f02bfd5d9fba9afbffae97b4dbedbdb53
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-09 12:33:20 +0100

    Add fetch all todos to the todo routes.

--- server/routes/todo/todo.routes.ts
+++ server/routes/todo/todo.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import isValidToken from '../../middleware/authentication/verifyToken.middleware'
-import { create, deleteOneTodo, updateOneTodo } from '../../controllers/todos.controller';
+import { create, deleteOneTodo, updateOneTodo, todos } from '../../controllers/todos.controller';
 import { validationRequestResults } from '../../middleware/validationErrorHandler/validationErrors.middleware'
 import { createTodoValidationCheck } from '../../middleware/todoValidation/todoValidationCheck.middleware'
 
 
 const Route = Router()
+Route.get("/", isValidToken, todos)
 Route.post("/new", isValidToken, createTodoValidationCheck, validationRequestResults, create);
-Route.patch("/:todoId", isValidToken, createTodoValidationCheck, updateOneTodo)
+Route.patch("/:todoId", isValidToken, updateOneTodo)
 Route.delete('/:todoId', isValidToken, deleteOneTodo)
 
 
