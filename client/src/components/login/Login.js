c3cc772aeb5d1959e206b6a7a38f07feab05302a
clartor <clara.tornblom@gmail.com>
2021-11-09 14:11:06 +0100

    style and stuff

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
+import '../stil/form.css'
 
 function Login() {
   // const username = "";
   return (
-    <div>
-      login login 
+    <div id="log">
+      <div className="formPage">
+      <h1> Logga in </h1>
     {/* <form onSubmit={handleSubmit} >
         <input ref={username} label="Username:" type="text" />
-        <input ref={email} label="Email:" type="email" />
         <input ref={password} label="Password:" type="password" />
-        <input ref={password} label="repeat Password:" type="password" />
         <div>
           <button style={submitStyle} type="submit">Submit</button>
         </div>
       </form> */}
+
+      <form id='userForm'>
+        <br/><input label="Username:" type="text" placeholder='Username'/>
+        <br/><input label="Password:" type="password" placeholder='Password'/>
+        <br/><div id='knappDiv'>
+          <button type="submit" id="skickaKnapp">Submit</button>
+        </div>
+      <a href='../../register/Register.js'>skapa konto här</a>
+      </form>
     </div>
+      </div>
   )
 }
 
--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import '../stil/form.css'
 
 function Register() {
   return (
-    <div>
-      <h1> Rehistreraaaaa </h1>
-      jsjs
+    <div id="reg">
+      <div className="formPage">
+      <h1> Registrera dig </h1>
+        {/* <form onSubmit={handleSubmit} >
+        <input ref={username} label="Username:" type="text" />
+        <input ref={email}    label="Email:" type="email" />
+        <input ref={password} label="Password:" type="password" />
+        <input ref={password} label="repeat Password:" type="password" />
+        <div>
+          <button style={submitStyle} type="submit">Submit</button>
+        </div>
+      </form> */}
+        <form id='userForm'>
+        <input label="Username:" type="text" placeholder='Username'/>
+        <input label="Email:" type="email" placeholder='Email'/>
+        <input label="Password:" type="password" placeholder='Password'/>
+        <input label="repeat Password:" type="password" placeholder='Repeat password'/>
+        <div id='knappDiv'>
+            <button type="submit" id="skickaKnapp">Submit</button>
+          </div>
+          <a href='../../login/LoginJs' >redan medlem? logga in här</a>
+        </form>
+      </div>
     </div>
   )
 }
