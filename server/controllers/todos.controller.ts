491dda1d93055cd04f67201875e729ca17d6fbd2
Josef Hedblom <hedblomjosef@gmail.com>
2021-11-09 12:32:02 +0100

    Clean up todo controller and add fetch all todos per user.

--- server/controllers/todos.controller.ts
+++ server/controllers/todos.controller.ts
@@ -1,21 +1,22 @@
 import { Request, Response } from 'express';
-import { createTodo, deleteTodo, updateTodo } from '../services/todo.service';
+import { createTodo, deleteTodo, updateTodo, fetchTodos } from '../services/todo.service';
+
+export const todos = async (req: Request, res: Response) => {
+  const todos = await fetchTodos(res.locals.user.id);
+  res.status(200).json(todos)
+}
 
 export const create = async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const todo = await createTodo(title);
+  const todo = await createTodo(req.body.title, res.locals.user.id);
   res.status(todo.status).json(todo.message)
 }
 
 export const updateOneTodo = async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const { todoId } = req.params;
-  const todo = await updateTodo(todoId, title);
+  const todo = await updateTodo(req.params.todoId, req.body.title);
   res.status(todo.status).json(todo.message)
 }
 
 export const deleteOneTodo = async (req: Request, res: Response) => {
-  const { todoId } = req.params;
-  const todo = await deleteTodo(todoId);
+  const todo = await deleteTodo(req.params.todoId);
   res.status(todo.status).json(todo.message)
 }
